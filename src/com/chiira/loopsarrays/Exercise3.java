21dc842e206d17277ee3264f0684bc5542f7e4b3
Kamania <kamaniajoseph@gmail.com>
2026-08-18 05:47:37 +0300

    Commit on exercise 3

--- src/com/chiira/loopsarrays/Exercise3.java
+++ src/com/chiira/loopsarrays/Exercise3.java
@@ -1,7 +1,6 @@
 package com.chiira.loopsarrays;
 
-public class Exercise2 {
-
+public class Exercise3 {
     public static void main(String[] args) {
         // declare an empty integer array of length = 3
         int numbers[] = new int[3];
@@ -11,9 +10,14 @@ public class Exercise2 {
             numbers[i] = 4;
         }
 
-        // print the contents to the console
+        // reassign the second value in the array to the number `17`
+        numbers[1] = 17;
+
+        // print the array to the console
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("Value at index " + i + " is " + numbers[i]);
         }
+
+
     }
 }
