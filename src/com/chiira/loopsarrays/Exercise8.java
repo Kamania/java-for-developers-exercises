8318f8d91e93b2a68301c09b22fba701ca4a1285
Kamania <kamaniajoseph@gmail.com>
2026-08-18 06:15:32 +0300

    Commit on exercise 8

---
+++ src/com/chiira/loopsarrays/Exercise8.java
@@ -0,0 +1,12 @@
+package com.chiira.loopsarrays;
+
+public class Exercise8 {
+    public static void main(String[] args) {
+        // create a for loop which goes from 0 to 10
+
+        for (int i = 0; i <= 10; i++) {
+            // print each value
+            System.out.println("Index at " + i);
+        }
+    }
+}
