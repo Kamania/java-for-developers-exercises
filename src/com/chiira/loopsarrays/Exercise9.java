ec193c778168d88bec81b2dbdbbd1830803e1427
Kamania <kamaniajoseph@gmail.com>
2026-08-18 06:20:12 +0300

    Commit on exercise 9

---
+++ src/com/chiira/loopsarrays/Exercise9.java
@@ -0,0 +1,12 @@
+package com.chiira.loopsarrays;
+
+public class Exercise9 {
+    public static void main(String[] args) {
+        // create a for loop which goes from 10 down to 0
+
+        // print each value
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Value at " + i);
+        }
+    }
+}
