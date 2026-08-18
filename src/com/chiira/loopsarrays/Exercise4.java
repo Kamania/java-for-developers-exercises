5ce7a250cde85fe06dbbbae50c86c5c8e5432975
Kamania <kamaniajoseph@gmail.com>
2026-08-18 05:50:27 +0300

    Commit on exercise 4 - ArrayIndexOutOfBoundsException error

---
+++ src/com/chiira/loopsarrays/Exercise4.java
@@ -0,0 +1,9 @@
+package com.chiira.loopsarrays;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        numbers[5] = 40; // throws ArrayIndexOutOfBoundsException error
+    }
+}
