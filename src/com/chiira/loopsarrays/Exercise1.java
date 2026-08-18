2162fd54987b8e46782233fb1644f3c698adc3ef
Kamania <kamaniajoseph@gmail.com>
2026-08-18 05:39:18 +0300

    Commit on exercise 1

---
+++ src/com/chiira/loopsarrays/Exercise1.java
@@ -0,0 +1,13 @@
+package com.chiira.loopsarrays;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        // declare an empty integer array of length = 3
+        int numbers[] = new int[3];
+
+        // print the contents to the console
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Value at index " + i + " is " + numbers[i]);
+        }
+    }
+}
