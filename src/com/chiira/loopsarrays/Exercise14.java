4304f71779cbbdd52d597e0abbde8f6862fecb95
Kamania <kamaniajoseph@gmail.com>
2026-08-21 05:49:20 +0300

    Implement exercise 14

---
+++ src/com/chiira/loopsarrays/Exercise14.java
@@ -0,0 +1,29 @@
+package com.chiira.loopsarrays;
+
+import java.util.Arrays;
+
+public class Exercise14 {
+    public static void main(String[] args) {
+    /*
+        Write a program that reverses an array of Strings
+        String[] content = {"you", "are", "how", "hello"}
+        Tip. Use for loop and you to start at highest index.
+        Output should be: hello how are you?. Note question mark at the end
+    */
+        String[] content = {"you", "are", "how", "hello"};
+        StringBuilder word = new StringBuilder();
+
+        for (int i = content.length - 1; i >= 0; i--) {
+            word.append(content[i]);
+
+            if (i > 0){
+                word.append(" ");
+            }
+        }
+
+        word.append("?");
+
+        System.out.println(word);
+
+    }
+}
