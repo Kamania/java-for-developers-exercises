5e58b453cc0d25effd44a71813306234e3f350ec
Kamania <kamaniajoseph@gmail.com>
2026-08-21 06:34:32 +0300

    conditions - Implement exercise 1

---
+++ src/com/chiira/conditionals/Exercise1.java
@@ -0,0 +1,20 @@
+package com.chiira.conditionals;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        // create an integer variable
+        int value1 = 12;
+
+        // create a second integer variable
+        int value2 = 12;
+
+        // create an `if` statement which compares the two variables and prints to the console if they are equal
+        if (value2 == value1) {
+            System.out.println("Value2: " + value2 + " and Value1: " + value1 + " are equal");
+        }else {
+            System.out.println("They are not equal");
+        }
+
+    }
+}
