d6f0155e716438226cc6ec495c83e11f304e7dd8
Kamania <kamaniajoseph@gmail.com>
2026-08-23 23:01:01 +0300

    conditions - Implement exercise 9

---
+++ src/com/chiira/conditionals/Exercise9.java
@@ -0,0 +1,13 @@
+package com.chiira.conditionals;
+
+public class Exercise9 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+        // create a for loop which goes from 0 to 20
+        // print each even value
+    }
+}
