46d2eb64ed2427b0e6205eb58093a25e2e96ca49
Kamania <kamaniajoseph@gmail.com>
2026-08-23 16:56:54 +0300

    conditions - Implement exercise 2

---
+++ src/com/chiira/conditionals/Exercise2.java
@@ -0,0 +1,19 @@
+package com.chiira.conditionals;
+
+public class Exercise2 {
+    // Question 2
+
+    public static void main(String[] args) {
+        // create a string variable
+        String text1 = "Text 1";
+
+        // create a second string variable which has the same value as the first
+        String text2 = "Text 1";
+
+        // create an `if` statement which compares the two variables and prints to the console if they are equal
+        if (text1 == text2){
+            System.out.println("Text 1 and Text 2 are equal");
+        }
+    }
+
+}
