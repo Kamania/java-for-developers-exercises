ff9c1f231859013d8c922da2466f26adfc8c6ed5
Kamania <kamaniajoseph@gmail.com>
2026-09-16 19:35:00 +0300

    method for summation of 2 values

---
+++ src/com/chiira/methods/Exercise1.java
@@ -0,0 +1,11 @@
+package com.chiira.methods;
+
+public class Exercise1 {
+    private static int sum(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(12, 34));
+    }
+}
