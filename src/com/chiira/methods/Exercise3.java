ff34292fce5b9e601f24a8d63ab5dbaf67f1c127
Kamania <kamaniajoseph@gmail.com>
2026-09-16 21:19:17 +0300

    method for comparison

---
+++ src/com/chiira/methods/Exercise3.java
@@ -0,0 +1,12 @@
+package com.chiira.methods;
+
+public class Exercise3 {
+
+    private static boolean compare(int val1, int val2){
+        return val1 == val2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(compare(12, 12));
+    }
+}
