d0891092432591edf080a13031b4a6da6faa0f86
Kamania <kamaniajoseph@gmail.com>
2026-09-16 19:38:33 +0300

    method for negation

---
+++ src/com/chiira/methods/Exercise2.java
@@ -0,0 +1,10 @@
+package com.chiira.methods;
+
+public class Exercise2 {
+    private static int negate(int value){
+        return -value;
+    }
+    public static void main(String[] args) {
+        System.out.println(negate(10));
+    }
+}
